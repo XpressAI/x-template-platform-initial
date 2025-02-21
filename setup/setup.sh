88cb17f19d098fe7099a0dbfcfff3f5209bac3fc
Eduardo Gonzalez <wmeddie@users.noreply.github.com>
2025-02-21 20:24:44 +0900

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,6 +5,7 @@ cd /data/home
 
 xircuits install converse
 xircuits install https://github.com/XpressAI/xai-xpressai
+xircuits install https://github.com/XpressAI/xai-agent
 xircuits install https://github.com/XpressAI/xai-pdf
 xircuits install https://github.com/XpressAI/xai-actors
 xircuits install https://github.com/XpressAI/xai-serpapi
