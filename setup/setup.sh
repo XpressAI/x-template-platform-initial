d32611327b6608941769337a9b2904e393a42d28
Eduardo Gonzalez <wmeddie@users.noreply.github.com>
2025-02-21 19:22:37 +0900

    Update setup.sh
    Install all the needed component libraries.

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,4 +4,9 @@ set -e
 
 xircuits install converse
 xircuits install https://github.com/XpressAI/xai-xpressai
+xircuits install https://github.com/XpressAI/xai-pdf
+xircuits install https://github.com/XpressAI/xai-actors
+xircuits install https://github.com/XpressAI/xai-serpapi
+xircuits install https://github.com/XpressAI/xai-requests
+
 
